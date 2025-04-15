960f175f46efbf97400e7ff421f82af841180424
hrithik210 <hrithiknayak7829@gmail.com>
2025-04-15 17:59:59 +0530

    added controllers for updates and get by id

--- controllers/product_controller.go
+++ controllers/product_controller.go
@@ -35,3 +35,39 @@ func getAllProducts(c *gin.Context) {
 
 	c.JSON(http.StatusOK, products)
 }
+
+func GetProductById(c *gin.Context) {
+	var product models.Product
+
+	id := c.Param("id")
+
+	if err := config.DB.First(&product, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "product not found"})
+		return
+	}
+	c.JSON(http.StatusOK, product)
+}
+
+func UpdateProduct(c *gin.Context) {
+	var product models.Product
+
+	var id string = c.Param("id")
+
+	if err := config.DB.First(&product, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "product not found"})
+		return
+	}
+
+	var input models.Product
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if input.IsMainCard {
+		config.DB.Model(&models.Product{}).Where("is_main_card = ?", true).Update("is_main_card = ?", false)
+	}
+
+	config.DB.Model(&product).Updates(&input)
+	c.JSON(http.StatusOK, product)
+}
